649eda723580cacff64ff32595cf06e54ef48e38
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-02-18 13:30:47 +0100

    terminado

---
+++ src/es/lvo/Main.java
@@ -0,0 +1,22 @@
+package es.lvo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero, factorial = 1;
+        Scanner entrada = new Scanner(System.in);
+
+        do {
+            System.out.print("Dame un número mayor que 0: ");
+            numero = entrada.nextInt();
+        } while (numero <= 0);
+
+        System.out.println("0! = 1");
+        for (int i = 1; i <= numero; i++){
+            factorial *= i;
+            System.out.println(i+ "! = " + factorial);
+        }
+    }
+}
